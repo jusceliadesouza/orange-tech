f18c24146111f0a77ff7cd083f4f4da8e0b78764
Juscélia de Souza <jusceliadesousa@gmail.com>
2022-10-25 19:10:45 -0300

    chore: limite de pokemons

--- pokedex-app/public/js/main.js
+++ pokedex-app/public/js/main.js
@@ -1,7 +1,8 @@
 const pokemonList = document.getElementById("pokemonList");
 const loadMoreButton = document.getElementById("loadMoreButton");
 
-const limit = 20;
+const maxRecords = 151;
+const limit = 10;
 let offset = 0;
 
 function loadPokemonItens(offset, limit) {
@@ -34,5 +35,14 @@ loadPokemonItens(offset, limit);
 
 loadMoreButton.addEventListener("click", () => {
   offset += limit;
-  loadPokemonItens(offset, limit);
+  const qtdRecordNextPage = offset + limit;
+
+  if (qtdRecordNextPage >= maxRecords) {
+    const newLimit = maxRecords - offset;
+    loadPokemonItens(offset, newLimit);
+
+    loadMoreButton.parentElement.removeChild(loadMoreButton)
+  }else{
+    loadPokemonItens(offset, limit);
+  }
 });

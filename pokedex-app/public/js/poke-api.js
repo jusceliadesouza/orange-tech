f41cbbdc91b45496302d5816ac71e008cafd9de0
Juscélia de Souza <jusceliadesousa@gmail.com>
2022-10-25 11:55:26 -0300

    chore: manipulação de múltiplas requisições em paralelo

--- pokedex-app/public/js/poke-api.js
+++ pokedex-app/public/js/poke-api.js
@@ -1,10 +1,16 @@
 const pokeApi = {};
 
+pokeApi.getPokemonDetail = (pokemon) => {
+  return fetch(pokemon.url).then((res) => res.json())
+};
+
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
-  const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
+  const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
 
   return fetch(url)
     .then((res) => res.json())
-    .then((jsonBody) => jsonBody.results);
-    // .catch((err) => console.error(err));
-};
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+    .then((detailRequests) => Promise.all(detailRequests))
+    .then((pokemonDetails) => pokemonDetails)
+}
